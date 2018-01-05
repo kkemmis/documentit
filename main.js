d672f134ef923611602d0362e1ff5f896966e650
Queen Bee <hi@kathykemmis.com>
2018-01-05 12:02:02 -0800

    set main.js to initialize the window, load index.htm and close appropriately when app is quit

--- main.js
+++ main.js
@@ -2,8 +2,38 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 
+// init win
 let win;
 
 function createWindow() {
-    win = new BrowserWindow({ width: 800, height: 600 });
+    // Create browser window
+    win = new BrowserWindow({ width: 800, height: 600, icon: });
+
+    // Load index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Open devtools
+    win.webContents().openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
 }
+
+// Run create window function
+app.on('ready', createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+
+    // Check if user is on a Mac
+    if (process.platform !== 'darwin') {
+        app.quit();
+
+    }
+
+});
